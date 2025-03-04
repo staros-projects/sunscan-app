6983121b892f3fd4ed16b646f1836218c9ce6c47
Guillaume Bertrand <gbe.io@pm.me>
2025-03-04 20:47:27 +0100

    fix android file download

--- utils/Helpers.js
+++ utils/Helpers.js
@@ -1,7 +1,7 @@
 import * as FileSystem from 'expo-file-system';
 import * as MediaLibrary from 'expo-media-library';
 import { Platform } from 'react-native';
-import { shareAsync } from 'expo-sharing';
+
 
 export const backend_current_version = '1.3.0';
 
@@ -12,94 +12,61 @@ export default function  firmareIsUpToDate(myContext) {
     return !myContext.backendApiVersion || parseInt(myContext.backendApiVersion.replaceAll('.','')) >= parseInt(backend_current_version.replaceAll('.',''))
 }
 
-const save = async (uri, fileName, mimetype) => {
-    // This has support only for Android 11 or more as expo requires to eject project in order to save to downloads
-    if (Platform.OS === 'android') {
-      const permissions =
-        await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-
-      if (permissions.granted) {
-        console.log(permissions);
-        const base64 = await FileSystem.readAsStringAsync(uri, {
-          encoding: FileSystem.EncodingType.Base64,
-        });
-        console.log(permissions.directoryUri, fileName, mimetype);
-        await FileSystem.StorageAccessFramework.createFileAsync(
-          permissions.directoryUri,
-          fileName.split('.')[0],
-          mimetype,
-        )
-          .then(async (resultUri) => {
-            await FileSystem.writeAsStringAsync(resultUri, base64, {
-              encoding: FileSystem.EncodingType.Base64,
-            });
-          })
-          .catch(console.log);
-      } else {
-        shareAsync(uri);
-      }
-    } else {
-      shareAsync(uri);
-    }
-  };
-
-export async function downloadAndroid(source, type) {
-    console.log('downloadAndroid', source, type, FileSystem.documentDirectory);
+async function saveAndroidFile(source, type) {
+    console.log('Downloading for Android:', source, type);
 
     try {
-        // Définir le nom du fichier
-        const baseFileName = `sunscan-image-${Date.now()}.${type}`;
-        const filename = `${FileSystem.cacheDirectory}${baseFileName}`;
+        const fileName = `sunscan-image-${Date.now()}.${type}`;
+        const filePath = `${FileSystem.cacheDirectory}${fileName}`;
+        const { uri: localUri } = await FileSystem.downloadAsync(source, filePath);
 
-        // Télécharger le fichier dans le cache
-        const { uri } = await FileSystem.downloadAsync(source, filename);
+        console.log('File downloaded at:', localUri);
 
-        save(uri, baseFileName, `image/${type}`)
-       
+        const directoryUri = FileSystem.StorageAccessFramework.getUriForDirectoryInRoot('Download');
+        const base64 = await FileSystem.readAsStringAsync(localUri, {
+            encoding: FileSystem.EncodingType.Base64,
+        });
+
+        const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync(directoryUri);
+        if (!permissions.granted) return false;
+
+        const newUri = await FileSystem.StorageAccessFramework.createFileAsync(
+            permissions.directoryUri,
+            fileName.split('.')[0],
+            `image/${type}`
+        );
+
+        await FileSystem.writeAsStringAsync(newUri, base64, { encoding: FileSystem.EncodingType.Base64 });
+        return true;
     } catch (error) {
-        console.error('Erreur lors de la sauvegarde de l’image :', error);
+        console.error('Error saving file on Android:', error);
         return false;
     }
 }
 
-
-export async function downloadIos(source, type)  {
-
-    if (permissionResponse.status !== 'granted') {
-        await requestPermission();
-    }
+async function saveIosFile(source, type) {
+    const permissionResponse = await MediaLibrary.requestPermissionsAsync();
+    if (permissionResponse.status !== 'granted') return false;
 
     try {
-       
-        const downloadPath = `${FileSystem.cacheDirectory}sunscan-image-${Date.now()}.`+type;
-        const { uri: localUrl } = await FileSystem.downloadAsync(
-            source,
-            downloadPath
-        );
+        const fileName = `sunscan-image-${Date.now()}.${type}`;
+        const filePath = `${FileSystem.cacheDirectory}${fileName}`;
+        await FileSystem.downloadAsync(source, filePath);
 
-        const asset = await MediaLibrary.createAssetAsync(downloadPath);
-
-        const album = await MediaLibrary.getAlbumAsync('Download');
-        if (album == null) {
+        const asset = await MediaLibrary.createAssetAsync(filePath);
+        let album = await MediaLibrary.getAlbumAsync('Download');
+        if (!album) {
             await MediaLibrary.createAlbumAsync('Download', asset, false);
         } else {
             await MediaLibrary.addAssetsToAlbumAsync([asset], album, false);
         }
         return true;
-    } catch (e) {
-        handleError(e);
+    } catch (error) {
+        console.error('Error saving file on iOS:', error);
         return false;
     }
-    return false;
 }
 
-// Function to download the current image
-export async function downloadSunscanImage(filename, type) {
-    if (Platform.OS === 'android') {
-        return await downloadAndroid(filename,type);
-    }
-    else{
-        return await downloadIos(filename, type);
-    }
+export async function downloadSunscanImage(source, type) {
+    return Platform.OS === 'android' ? await saveAndroidFile(source, type) : await saveIosFile(source, type);
 }
-
